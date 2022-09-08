b43a34e0c3a49f4d38d9f15a8dba44520f240bdb
AmiruC <83331140+AmiruC@users.noreply.github.com>
2022-09-08 19:31:32 +1000

    Add files via upload

---
+++ SEPTA1ShopQ/src/test/java/com/s3898594/septa1shopq/Septa1ShopQApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s3898594.septa1shopq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Septa1ShopQApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
